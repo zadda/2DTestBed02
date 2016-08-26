ef406d02b583c4ed9764e0351d5b90e08e1346c3
mike_b <atm89@hotmail.com>
2016-08-26 11:47:33 +0200

    jet bombexplosion added
    via animation, destroy time not finetuned yet

---
+++ Assets/BombExplosion.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombExplosion : MonoBehaviour 
+{
+
+    [SerializeField]
+    private GameObject explosion;
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Instantiate(explosion, transform.position, Quaternion.identity);
+        GameObject explosie = Instantiate(explosion, transform.position, Quaternion.identity) as GameObject;
+        Destroy(explosie, 5f);
+        Destroy(gameObject);
+    }
+}
