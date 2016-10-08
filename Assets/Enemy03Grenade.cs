2ccdd0cf75588eb1db00fd3750dda8a846a3f609
mike_b <atm89@hotmail.com>
2016-10-08 13:47:24 +0200

    gasgrenade small fixes

--- Assets/Enemy03Grenade.cs
+++ Assets/Enemy03Grenade.cs
@@ -11,21 +11,37 @@ public class Enemy03Grenade : MonoBehaviour
     [SerializeField]
     private GameObject grenade;
 
+    [SerializeField]
+    private Transform barrel;
+
+    [SerializeField]
+    private GameObject flares;
 
     [SerializeField]
     private Transform launchPosition;
+
+    private Vector3 enemyPosition;
 
     [SerializeField]
     private GameObject gas;
     
     public float health = 200;
+    [SerializeField]
+    private GameObject healthBar;
 
     public static bool stopAttacking = false;
     
-    private bool grenadeFired = false;
+    [SerializeField]
+    private bool grenadeFired = true;
 
-	// Use this for initialization
-	void Start () 
+    int launchSpeed = -70;
+    int grenadesFired = 0;
+
+    private float timerCountdown = 2f;
+
+
+    // Use this for initialization
+    void Start () 
 	{
 	
 	}
@@ -33,18 +49,80 @@ public class Enemy03Grenade : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-        if (!grenadeFired)
+        timerCountdown -= Time.deltaTime;
+
+
+        if (!grenadeFired && timerCountdown <= 0)
         {
-            Fire();
+           Fire();
         }
-	}
+        healthBar.transform.localScale = new Vector3(health / 200, 1, 1);
+
+        CheckHealth();
+
+    }
+
+    void CheckHealth()
+    {
+
+
+        if (health <= 50 && EnemyHeli.heliCalled == false)
+        {
+            // launch Flare and call HeliSupport
+            EnemyHeli.heliCalled = true;
+
+            enemyPosition = transform.position;
+
+            //ceaseFire = true;
+            //sprite.flipX = true;
+
+            GameObject flare = Instantiate(flares, barrel.position, Quaternion.identity) as GameObject;
+            //transform.Translate(Vector3.up * 220.5f * Time.deltaTime);
+            flare.GetComponent<Rigidbody2D>().velocity = new Vector3(0, 30, 0); // bullet speed in -X position
+
+            //move enemy
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(40, 0, 0);
+        }
+
+        DefensivePositionReached();
+    }
+
+    void DefensivePositionReached()
+    {
+
+        Rigidbody2D rBody = GetComponent<Rigidbody2D>();
+
+
+
+        //if (defensivePositionReached == true && transform.position.x >= defensiveObstacle.position.x + 10)
+        //{
+        //    rBody.velocity = new Vector3(0, 0, 0);
+        //    // rBody.constraints = RigidbodyConstraints2D.FreezePositionX;
+
+        //    sprite.flipX = false;
+        //    ceaseFire = false;
+        //    defensivePositionReached = false;
+        //    stopMoving = true;
+        //}
+    }
+
 
     void Fire()
     {
-        grenadeFired = true;
+
+        timerCountdown = 2;
+        grenadesFired++;
+        if (grenadesFired == 5)
+        {
+            grenadeFired = true;
+        }
+        
         GameObject granaat = Instantiate(grenade, launchPosition.position, Quaternion.identity) as GameObject;
         //granaat.transform.rotation = Quaternion.Euler(0, 0, 351);
-        granaat.GetComponent<Rigidbody2D>().velocity = new Vector3(-70, launchPosition.rotation.z * 100 + 2, 0);
+        granaat.GetComponent<Rigidbody2D>().velocity = new Vector3(launchSpeed, launchPosition.rotation.z * 100 + 2, 0);
+        launchSpeed -= 20;
+
+
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -59,5 +137,32 @@ public class Enemy03Grenade : MonoBehaviour
        
         Destroy(explosie, 10f);
         //Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject objectCollidedwith = collision.gameObject;
+
+        //get amount of damage
+
+        //check first if we are colliding with a Player Projectile 
+        //if so, get Damage value from Player Projectile
+
+        if (objectCollidedwith.GetComponent<PlayerProjectileDamage>())
+        {
+            float damage = objectCollidedwith.GetComponent<PlayerProjectileDamage>().damage;
+
+            health -= damage;
+            Destroy(objectCollidedwith); // destroy Player projectile
+        }
+
+        //kill the enemy
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        // Hit by FlashGrenade -> cease fire flip sprite
+
     }
 }
--- Assets/_Scripts/Enemies/GasGrenade.cs
+++ Assets/_Scripts/Enemies/GasGrenade.cs
@@ -12,8 +12,8 @@ public class GasGrenade : MonoBehaviour
 
     [SerializeField]
     private GameObject explosion;
-    
-    
+
+
 
     void Update()
     {
@@ -25,27 +25,45 @@ public class GasGrenade : MonoBehaviour
 
     void Throw()
     {
-       
 
-            GameObject granaat = Instantiate(grenade, grenadeStartPosition.transform.position, Quaternion.identity) as GameObject;
-            granaat.GetComponent<Rigidbody2D>().velocity = new Vector3(-120, -15, 0);
-            granaat.GetComponent<Rigidbody2D>().rotation = -5;
-       
+
+        GameObject granaat = Instantiate(grenade, grenadeStartPosition.transform.position, Quaternion.identity) as GameObject;
+        granaat.GetComponent<Rigidbody2D>().velocity = new Vector3(-120, -15, 0);
+        granaat.GetComponent<Rigidbody2D>().rotation = -5;
+
     }
 
-    void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerEnter2D(Collider2D collider)
     {
         //zorgt ervoor dat de rook hoog genoeg zichtbaar is, anders deels onder de grond
         Vector3 positie = transform.position;
         positie += new Vector3(0, 8, 0);
 
         //Instantiate(explosion, transform.position, Quaternion.identity);
-        GameObject explosie = Instantiate(explosion, positie, Quaternion.identity) as GameObject;
 
-     
-
-
-        Destroy(explosie, 10f);
-        Destroy(gameObject);
+        if (!collider.tag.Equals("Enemy"))
+        {
+            GameObject explosie = Instantiate(explosion, positie, Quaternion.identity) as GameObject;
+            Destroy(explosie, 10f);
+            Destroy(gameObject);
+        }
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //zorgt ervoor dat de rook hoog genoeg zichtbaar is, anders deels onder de grond
+        Vector3 positie = transform.position;
+        positie += new Vector3(0, 8, 0);
+
+        //Instantiate(explosion, transform.position, Quaternion.identity);
+        if (!collision.collider.tag.Equals("Enemy"))
+        {
+            GameObject explosie = Instantiate(explosion, positie, Quaternion.identity) as GameObject;
+            Destroy(explosie, 10f);
+            Destroy(gameObject);
+        }
+
+    }
+
+
 }
