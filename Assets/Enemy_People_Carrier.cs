29f4525972001925cb9c8f9e1bcdb39a88a8a0bd
mike_b <atm89@hotmail.com>
2016-11-01 11:18:53 +0100

    Enemy People carrier heli added

---
+++ Assets/Enemy_People_Carrier.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_People_Carrier : MonoBehaviour 
+{
+    [SerializeField]
+    private GameObject enemyToSpawn;
+
+    [SerializeField]
+    private Transform spawnPoint;
+
+
+    // Use this for initialization
+    void Start () 
+	{
+        GameObject enemy = Instantiate(enemyToSpawn, spawnPoint.transform.position, Quaternion.Euler(0f, 0f, 0f)) as GameObject;
+    }
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
